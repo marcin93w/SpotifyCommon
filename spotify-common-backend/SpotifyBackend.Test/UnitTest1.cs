7e942c3484d66cbb88be453d746df8992ed72719
PAWELPC\Grzechu <grzechu435@gmail.com>
2017-07-20 22:45:00 +0200

    add tests

---
+++ spotify-common-backend/SpotifyBackend.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.IO;
+using Xunit;
+using Newtonsoft.Json;
+
+namespace SpotifyBackend.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
